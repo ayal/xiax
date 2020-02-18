7f2b1500cf5819aec1f83213e32f956804377769
ayal <ayalgelles@gmail.com>
2020-02-18 17:39:27 +0100

    Updates

---
+++ precache-manifest.edd6d41f76b8d16fb27d7fa35bd1b765.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d26ba923699e4bd7975bf92b1b1a562f",
+    "url": "/xiax/index.html"
+  },
+  {
+    "revision": "9ab29081da55ab61a200",
+    "url": "/xiax/static/css/main.8cccc650.chunk.css"
+  },
+  {
+    "revision": "5585f95ec2c82be49c73",
+    "url": "/xiax/static/js/2.d0125111.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/xiax/static/js/2.d0125111.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ab29081da55ab61a200",
+    "url": "/xiax/static/js/main.6c1d785a.chunk.js"
+  },
+  {
+    "revision": "1b08880be90367de3c00",
+    "url": "/xiax/static/js/runtime-main.86dd4ac8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/xiax/static/media/logo.5d5d9eef.svg"
+  }
+]);
